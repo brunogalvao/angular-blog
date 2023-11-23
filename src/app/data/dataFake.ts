e9d79278f09020cb20ce5a4b0faaccc932f72ddd
Bruno Galvao <bruno_galvao@outlook.com>
2023-11-23 15:25:34 -0300

    fix: layout

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,7 +2,15 @@ export const dataFake = [
   {
     id: '1',
     title: 'Nova versão do Angular',
-    description: 'Aqui será a descrição do meu primeiro Big Card',
+    description: `Angular é uma plataforma de desenvolvimento, construída em TypeScript.
+
+    Como plataforma, o Angular inclui:
+
+    Uma estrutura baseada em componentes para criar aplicativos da web escaláveis
+    Uma coleção de bibliotecas bem integradas que cobrem uma ampla variedade de recursos, incluindo roteamento, gerenciamento de formulários, comunicação cliente-servidor e muito mais
+    Um conjunto de ferramentas de desenvolvedor para ajudá-lo a desenvolver, construir, testar e atualizar seu código
+    Com o Angular, você está aproveitando uma plataforma que pode escalar de projetos de desenvolvedor único a aplicativos de nível empresarial. O melhor de tudo é que o ecossistema Angular consiste em um grupo diversificado de mais de 1,7 milhão de desenvolvedores, autores de bibliotecas e criadores de conteúdo.`,
+    litleDescription: `Angular é uma plataforma de desenvolvimento, construída em TypeScript.`,
     photoCover:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjYgG4y2Yw8uHNn_mva2Dq1E0gIBK32Th3uR1HlMsVKatGFrba091PZIQTL5MUA4HCmxQ&usqp=CAU',
   },
@@ -10,6 +18,7 @@ export const dataFake = [
     id: '2',
     title: 'Novidades na Marvel',
     description: 'Aqui será o meu segundo Big Card',
+    litleDescription: `Angular é uma plataforma de desenvolvimento, construída em TypeScript.`,
     photoCover:
       'https://t.ctcdn.com.br/bNnB3OpzJZuIVSqpsOY9dBQzGF4=/640x360/smart/i604548.jpeg',
   },
@@ -17,6 +26,7 @@ export const dataFake = [
     id: '3',
     title: 'Loki tem a segunda temporada confirmada',
     description: 'Aqui será o meu segundo Big Card',
+    litleDescription: `Angular é uma plataforma de desenvolvimento, construída em TypeScript.`,
     photoCover:
       'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2021/07/loki-disney-e1626286003674.jpg',
   },
@@ -24,6 +34,7 @@ export const dataFake = [
     id: '4',
     title: 'As Marvels terá Teyonah Parris como Monica Rambeau',
     description: 'Tudo sobre as Marvels',
+    litleDescription: `Angular é uma plataforma de desenvolvimento, construída em TypeScript.`,
     photoCover:
       'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/11/as_marvels_capa__qc22wv23-1210x544.jpg',
   },
@@ -31,6 +42,7 @@ export const dataFake = [
     id: '5',
     title: 'Kang, o Conquistador, é o grande vilão da Marvel',
     description: 'Kang, o Conquistador, é o grande vilão da Marvel',
+    litleDescription: `Angular é uma plataforma de desenvolvimento, construída em TypeScript.`,
     photoCover:
       'https://cinepop.com.br/wp-content/uploads/2023/01/kang-o-conquistador-homem-formiga-3.webp',
   },
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css', './home.responsive.component.css'],
 })
 export class HomeComponent implements OnInit {
+  homePhotoCover: string = '';
+  homeTitle: string = '';
+  homeDescription: string = '';
+
+  dataFake = dataFake;
+
   constructor() {}
 
   ngOnInit(): void {}
